615ed7c138d631d1455f7c9e2524161c60be343f
samhithareddyy <samhithareddyyy@gmail.com>
2024-09-11 12:13:55 +0530

    Add files via upload

--- sam a1 q39.c
+++ sam a1 q39.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-int main() {
-
-int n, i;
-
-printf("Enter a positive integer: ");
-scanf("%d", &n);
-if (n < 2) { printf("%d is neither a prime number nor a composite number\n", n);
+int main() {float a=1; 
+	float result=0;
+	for(int i=1;i<=4;i++){
+	
+	a*=i;
+	   result+=i/a;
+	}
+	
+printf("result is %f", result);
+    return 0;
 }
-for (i=2; i<=n/2; i++) { if (n%i!=0) { printf("prime number"); 
-}
- else { printf("%d is composite number\n", n);
-}
-}
-return 0;}
