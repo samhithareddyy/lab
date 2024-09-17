f531141413cc5f280a8138d5760932a7c685d3e6
samhithareddyy <samhithareddyyy@gmail.com>
2024-09-18 00:25:34 +0530

    Update sam a1 q32.c

--- sam a1 q32.c
+++ sam a1 q32.c
@@ -9,26 +9,19 @@ int main() {
     printf("Enter 1st number ");
     scanf("%d", &number);
     max1 = number;  
-    max2 = -99999999; 
 
     for (i = 1; i < n; i++) {
         printf("Enter number %d: ", i + 1);
         scanf("%d", &number);
 
-        if (number > max1) {
+        if (number > max1) 
             max2 = max1; 
             max1 = number;  
-        } else if (number > max2 && number != max1) {
+        } else if (number != max1) {
             max2 = number;  
         }
     }
-
-    if (max2 == -99999999) {
-        printf("There is no second maximum number.\n");
-    } else {
         printf("The maximum number is: %d\n", max1);
         printf("The second maximum number is: %d\n", max2);
-    }
-
     return 0;
 }
