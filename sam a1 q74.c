366673fd90dbfedbe51331eb7c451fef9b725b07
samhithareddyy <samhithareddyyy@gmail.com>
2024-11-16 13:15:22 -0500

    Update sam a1 q74.c

--- sam a1 q74.c
+++ sam a1 q74.c
@@ -1,29 +1,35 @@
 #include<stdio.h>
-// Given are one dimensional arrays A and B which are sorted in ascending order. 
-// Write a program to merge them into a single sorted array C that contains every 
-// item form array A and B, in ascending order. 
+ 
 int main() {
 
-    int Num1[5]={1,3,5,7,9}, Num2[5]={2,4,6,8,10}, Num3[10], i, j, temp=0;
+    int Num1[4]={1,3,5,7}, Num2[5]={2,4,6,8,10}, a[9], i, j, temp=0;
     
-    for(i=0; i<5; i++) {
-        Num3[i]=Num1[i];
-        Num3[i+5]=Num2[i];
-    }
+    for(i=0; i<4; i++) 
+        a[i]=Num1[i];
+    for(i=0;i<5;i++)
+        a[i+4]=Num2[i];
+    
 
-    for(i=0; i<10; i++) {
-        for(j=9; j>i; j--) {
-            if(Num3[i] > Num3[j]) {
-                temp=Num3[i];
-                Num3[i]=Num3[j];
-                Num3[j]=temp;
+    for (int i = 0; i < 9 - 1; i++) {
+        int min = i;  
+        for (int j = i + 1; j < 9; j++) {
+            if (a[j] < a[min]) {
+                min= j;
             }
+        }
+        
+        if (min != i) {
+            int temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
         }
     }
 
-    for(i=0; i<10; i++) {
-        printf("%d \t", Num3[i]);
+    
+    for (int k = 0; k < 9; k++) {
+        printf("%d ", a[k]);
     }
+
 
     return 0;
 }
