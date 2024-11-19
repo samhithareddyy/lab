d80d1fe9e76562900f41ef68af40596aad12cb3f
samhithareddyy <samhithareddyyy@gmail.com>
2024-11-19 02:04:53 -0500

    Update a1 q71.cpp

--- a1 q71.cpp
+++ a1 q71.cpp
@@ -1,23 +1,36 @@
 #include<stdio.h>
 int main(){
-	int a[10]={5,9,17,23,25,45,59,63,71,89};
-	int n=sizeof(a)/sizeof(a[0]);
+	int a[4],n;
 	
-	int l=0,r=n-1;
-	int d;
-	scanf("%d",&d);
+	for(int i=0;i<4;i++)
+		scanf("%d",&a[i]);
+	
+	printf("enter no.of positions");
+	scanf("%d",&n);
+	
+	for(int i=0;i<n;i++){
+		for(int j=0;j<4-1;j++){
+			int temp=a[0];
+			a[0]=a[4-j-1];
+			a[4-j-1]=temp;
+		}
+	}
+	
+	
+	int l=0,r=4-1;
 	
 	for(int i=0;;i++){
-		int m=a[(l+r)/2];
-		
-		if(m==d){
-		printf("found at %d",(l+r)/2+1);
-		break;}
-		
-		else if(m<d)
-			l=(l+r)/2+1;
-			
-		else
-			r=(l+r)/2-1;
+	int m=a[(l+r)/2];
+	if(m<a[(l+r)/2-1])
+	{
+		printf(" pivot element found at %d",(l+r)/2+1);
+		break;
 	}
-}
+	
+	else if(m<a[r])
+		r=(l+r)/2-1;
+		
+	else if(m>a[r])
+		l=(l+r)/2+1;
+	
+	}}
